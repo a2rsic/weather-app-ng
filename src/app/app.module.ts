130dfc277780e5cb6ac79c1122061356aea7f524
Ana <angy.aa@gmail.com>
2019-03-19 11:33:15 +0100

    Add weather-forecast page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WeatherSearchComponent } from './weather-search/weather-search.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WeatherSearchComponent
+    WeatherSearchComponent,
+    WeatherForecastComponent
   ],
   imports: [
     BrowserModule
